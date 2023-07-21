fdcc1b04ff64fb53aa8f2a48bdff121b027412be
Luiz Tessarolli <tessarolli@gmail.com>
2023-07-21 11:48:14 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+
+    builder.Services.AddHttpClient("StarWars", c => c.BaseAddress = new Uri("https://rickandmortyapi.com/graphql"));
+
+    builder.Services
+        .AddGraphQLServer()
+        .AddRemoteSchema("StarWars");
+}
+
+var app = builder.Build();
+{
+    app.MapGraphQL();
+
+    app.Run();
+}
